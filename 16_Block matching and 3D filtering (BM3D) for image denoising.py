f037abe9cf931bf433fa873d09d315184a2f6085
ImrulkayeSifat <kayesimrul224@gmail.com>
2022-08-30 03:39:55 +0600

    Block matching and 3D filtering (BM3D) for image denoising

---
+++ 16_Block matching and 3D filtering (BM3D) for image denoising.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 30 03:25:00 2022
+
+@author: timjim
+"""
+
+from skimage import io, img_as_float
+import bm3d
+import cv2
+
+noisy_img = img_as_float(io.imread("images/Osteosarcoma_01_25Sigma_noise.tif", as_gray=True))
+
+BM3D_denoised_image = bm3d.bm3d(noisy_img, sigma_psd=0.2, stage_arg=bm3d.BM3DStages.HARD_THRESHOLDING)
+
+
+"""
+bm3d library is not well documented yet, but looking into source code....
+sigma_psd - noise standard deviation
+stage_arg: Determines whether to perform hard-thresholding or Wiener filtering.
+stage_arg = BM3DStages.HARD_THRESHOLDING or BM3DStages.ALL_STAGES (slow but powerful)
+All stages performs both hard thresholding and Wiener filtering. 
+"""
+
+cv2.imshow("Original", noisy_img)
+cv2.imshow("Denoised", BM3D_denoised_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
